e3fe200385b0535b15d01c45a3daa77e26374e31
isuke <isuke770@gmail.com>
2018-10-23 17:34:38 +0900

    fix 'no method matching parse' error

--- src/rw_stat_main.jl
+++ src/rw_stat_main.jl
@@ -1,7 +1,7 @@
 include("rw_stat.jl")
 
-dim = length(ARGS)>0 ? parse(ARGS[1]) : 1
-num = length(ARGS)>1 ? parse(ARGS[2]) : 1000
+dim = length(ARGS)>0 ? parse(Int, ARGS[1]) : 1
+num = length(ARGS)>1 ? parse(Int, ARGS[2]) : 1000
 nsteps = length(ARGS)>2 ? parse(Int, ARGS[3]) : 1000
 filename = length(ARGS)>3 ? ARGS[4] : ""
 
